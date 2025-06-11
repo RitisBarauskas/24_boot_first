e2c16163a35dda2f92124ab5a412b677bdc60a58
Ritis Barauskas <perm300ru@yandex.ru>
2025-06-11 19:49:30 +0300

    upd print_hello

--- core/super_iters.py
+++ core/super_iters.py
@@ -1,11 +1,28 @@
+from random import shuffle
+
+
 def magic_printer(count=10):
     for item in range(count):
         print(f'Magic #{item}')
 
 
-def print_hello(names: list = None):
+def print_hello(
+        names: list = None,
+        exclude_names: list = None,
+        is_random: bool = False,
+):
     if names is None:
         names = ['24 bootcamp']
 
+    if exclude_names is None:
+        exclude_names = []
+
+    if is_random:
+        shuffle(names)
+
     for name in names:
+        if name in exclude_names:
+            print(f'Name {name} was excluded')
+            continue
+
         print(f'Hello, {name}!')
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from core.super_iters import print_hello
 
 
 def main():
-    names = ['Alexandra', 'Nikita', 'Julya', 'Kristina']
-    print_hello(names)
+    names = ['Alexandra', 'Nikita', 'Julya', 'Kristina', 'Ritis']
+    print_hello(names=names, exclude_names=['Ritis'], is_random=True)
 
 
 if __name__ == '__main__':
